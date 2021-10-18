7ce28389c397b1bd9f30a34a9f2398933bd5b098
Emilee Oquist <ecmo@BKST-00552LR0.pugetsound.edu>
2021-10-17 17:12:23 -0700

    updated cheer to change img, removed notes, updated some comments

--- app.js
+++ app.js
@@ -3,27 +3,6 @@
  * @author Emilee Oquist
  */
 
-/**
-I’d start by ensuring that each box in the box score has an identifier or is uniquely named.
-To access the DOM, query for all the boxes, and store them in an appropriate data structure so that they’re easily
-identified and updated later in your code. I think a JS object makes sense here, but that I’ll leave it up to you.
-
-After you’ve got DOM access to all the boxes, run some test code to see if you can get the values to change.
-For instance, just set a node’s innerHTML property to a different value.
-
-Now move on to the control center. Run a query for the 6 manipulable elements. Recall that each option in a drop-down
-menu is specified in the value attribute.
-
-Images and Sounds: To play sounds using JavaScript is quite simple: look into the Audio class.
-Four music files (mp3) have been provided to you.
-
-When the left button (the one with music notes) is pressed, randomly play one of: charge1.mp3, charge2.mp3,
-or buildup.mp3. Also randomly display one of the images to the right of the box score.
-When the right button (the one with a keyboard) is pressed, play the Chicken Dance: chickendance.mp3,
-and always display the sdchicken.jpg to the right of the box score. Pressing on another button before the song
-has finished may cause the music to overlap. This is okay (for now) because we don’t know how to wait for the song to end.
- */
-
 // later idea: fill arrays with pathnames
 var $imgs = ["images/ups.png", "images/megaphone.png", "images/sdchicken.jpg"],
     $sfx = ["sounds/buildup.mp3", "sounds/charge1.mp3", "sounds/charge2.mp3"],
@@ -62,6 +41,7 @@ document.getElementById("run--").addEventListener("click", function () {
 
 // on click events for the music
 document.getElementById("cheer").addEventListener("click", function () {
+    document.querySelector(".icon").src = $imgs[Math.round(Math.random())]; // random image (that isn't the chicken image)
     var ransong = $sfx[Math.floor(Math.random() * $sfx.length)]; // grab random cheer
     (new Audio(ransong)).play();
 });
